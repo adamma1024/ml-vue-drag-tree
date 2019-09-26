5320a3f8aa246ace763b15c40df13738df790d4b
qq240814476 <240814476@qq.com>
2019-09-26 22:25:16 +0800

    a

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,12 +1,12 @@
 var path = require('path')
 var utils = require('./utils')
-var webpack = require('webpack')
 var merge = require('webpack-merge')
 var baseWebpackConfig = require('./webpack.base.conf')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 
 var webpackConfig = merge(baseWebpackConfig, {
+  mode:'production',
   module: {
     rules: utils.styleLoaders({
       sourceMap: true,
@@ -31,15 +31,6 @@ var webpackConfig = merge(baseWebpackConfig, {
     loadsh: 'loadsh'
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {     //压缩代码
-          dead_code: true,    //移除没被引用的代码
-          warnings: false,     //当删除没有用处的代码时，显示警告
-          loops: true //当do、while 、 for循环的判断条件可以确定是，对其进行优化
-      },
-      sourceMap: true,
-      except: ['$super', '$', 'exports', 'require']    //混淆,并排除关键字
-    }),
     // extract css into its own file
     new ExtractTextPlugin({
       filename: 'vue-drag-tree.min.css'
